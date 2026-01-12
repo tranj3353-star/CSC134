1ee2955bb891c0c19181b5e7b8d9524ca65bbbf4
tranj3353-star <tranj3353@student.faytechcc.edu>
2026-01-12 19:39:38 +0000

    Added exponent functionality

--- m1t1_Tran.cpp
+++ m1t1_Tran.cpp
@@ -1,11 +1,31 @@
-// CSC 134
-// M1T1 (Module 1, tutorial 1)
+//CSC 134
+//M1T1 (Module 1, tutorial 1)
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    cout << "hello world!" << endl;
-    cout << endl;
+    cout << "hello world!" << endl << endl;
+
+    //Get and display name
+    string name;
+    cout << "Enter your first name: ";
+    cin >> name; //Input
+    cout << "Name: " + name << endl << endl;
+
+    //Get and display number
+    int num1;
+    cout << "Enter whole number: ";
+    cin >> num1;
+    cout << "Num: " + to_string(num1) << endl;
+
+    //Get exponent from 1st number
+    int exponent;
+    cout << "Enter whole number for exponentiation: ";
+    cin >> exponent;
+    int result = pow(num1, exponent);
+    cout << num1 << " raised to " << exponent << " is " << result << endl;
+
     return 0;
 }
