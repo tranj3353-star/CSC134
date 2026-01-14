d61c9552e071563a2dfe9207869f5ff0619d6efc
tranj3353-star <tranj3353@student.faytechcc.edu>
2026-01-14 18:58:09 +0000

    Added input asking

--- m1lab_Tran.cpp
+++ m1lab_Tran.cpp
@@ -15,12 +15,19 @@ using namespace std;
 int main() {
     string item = "bananas";
     int item_count = 100;
+    int purchased;
     double price_each = 0.79;
-    double total_price = item_count * price_each;
 
     cout << "Welcome to our " << item << " store!" << endl;
     cout << "We have " << item_count << " " << item << "!" << endl;
     cout << "They cost $" << price_each << " each!" << endl;
+
+    cout << "How many would you like to buy?: ";
+    cin >> purchased;
+
+    double total_price = purchased * price_each;
+
+    cout << "You purchased " << purchased << " " << item << endl;
     cout << "Total price is $" << total_price << endl;
 
     return 0;
