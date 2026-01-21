55302bd73ab9ea6d3f371b83bd98bc82d54ee1c8
tranj3353-star <tranj3353@student.faytechcc.edu>
2026-01-21 18:22:00 +0000

    move stuff around

---
+++ m2/m2t1_Tran.cpp
@@ -0,0 +1,37 @@
+//CSC 134
+//M1LAB - Sales
+//Justin Tran
+//1/14/26
+
+/*
+We're selling bananas
+Tell users how much you have
+How much each costs
+Total cost
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    string item_name;
+    int item_count;
+    int purchased;
+    double price_each;
+
+    cout << setprecision(2) << fixed;
+
+    cout << "Welcome to our " << item_name << " store!" << endl;
+    cout << "We have " << item_count << " " << item_name << "!" << endl;
+    cout << "They cost $" << price_each << " each!" << endl;
+
+    cout << "How many would you like to buy?: ";
+    cin >> purchased;
+
+    double total_price = purchased * price_each;
+
+    cout << "You purchased " << purchased << " " << item_name << endl;
+    cout << "Total price is $" << total_price << endl;
+
+    return 0;
+}
