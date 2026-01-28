35a970230e74fab7f6cd21428ef41ac3cd85fc70
tranj3353-star <tranj3353@student.faytechcc.edu>
2026-01-28 18:47:53 +0000

    M2Lab1 Interpretation

---
+++ m2/m2Lab1_tran.cpp
@@ -0,0 +1,39 @@
+/*
+CSC 134
+M2LAB1 - Crates
+Justin Tran
+1/28/26
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    //Initialize variables
+    double length, width, height, volume; //Feet
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    double cost, customer_price, profit;
+
+    //Inputs
+    cout << "Crate Purchase Program" << endl;
+    cout << "Enter the size of your crate" << endl;
+    cout << "Length?: ";
+    cin >> length;
+    cout << "Width?: ";
+    cin >> width;
+    cout << "Height?: ";
+    cin >> height;
+
+    //Calculations
+    volume = length * width * height;
+
+    //Outputs
+    cout << setprecision(2) << fixed;
+    cout << "\n ----- Crate Info ----- \n";
+    cout << "Volume: " << volume << " cubic ft. " << endl;
+
+    return 0;
+}
